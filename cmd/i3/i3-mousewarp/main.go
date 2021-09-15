fb3dfb5a0e675138f770c12f590afc23d7f3dbc0
Alex Ermolov <aaermolov@gmail.com>
2021-09-15 13:05:54 +0300

    i3-mousewarp: parameterized cursor position coordinates

--- cmd/i3/i3-mousewarp/main.go
+++ cmd/i3/i3-mousewarp/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"sync"
 
+	"github.com/urfave/cli/v2"
 	"github.com/wiedzmin/toolbox/impl"
 	"github.com/wiedzmin/toolbox/impl/shell"
 	"go.i3wm.org/i3"
@@ -13,7 +15,7 @@ import (
 var wg sync.WaitGroup
 var logger *zap.Logger
 
-func handleWindows() {
+func handleWindows(ctx *cli.Context) {
 	defer wg.Done()
 	recv := i3.Subscribe(i3.WindowEventType)
 	defer recv.Close()
@@ -21,19 +23,55 @@ func handleWindows() {
 		ev := recv.Event().(*i3.WindowEvent)
 		switch ev.Change {
 		case "focus":
-			// TODO: parameterize windows fraction sizes below (/3)
-			cursorX := ev.Container.Rect.X + ev.Container.Rect.Width/3
-			cursorY := ev.Container.Rect.Y + ev.Container.Rect.Height/3
-			_, _ = shell.ShellCmd(fmt.Sprintf("xdotool mousemove %d %d", cursorX, cursorY), nil, nil, false, false)
+			cursorX := float64(ev.Container.Rect.X) + float64(ev.Container.Rect.Width)*ctx.Float64("x-frac")
+			cursorY := float64(ev.Container.Rect.Y) + float64(ev.Container.Rect.Height)*ctx.Float64("y-frac")
+			_, _ = shell.ShellCmd(fmt.Sprintf("xdotool mousemove %f %f", cursorX, cursorY), nil, nil, false, false)
 		}
 	}
+}
+
+func perform(ctx *cli.Context) error {
+	wg.Add(1)
+	go handleWindows(ctx)
+	wg.Wait()
+	return nil
+}
+
+func createCLI() *cli.App {
+	app := cli.NewApp()
+	app.Name = "i3-mousewarp"
+	app.Usage = "Warps mouse cursor to the position of recently activated window"
+	app.Description = "i3-desktops"
+	app.Version = "0.0.1#master"
+
+	app.Flags = []cli.Flag{
+		&cli.Float64Flag{
+			Name:     "x-frac",
+			Aliases:  []string{"x"},
+			Usage:    "X fraction for cursor placement between 0 and 1, 0.5 denotes window center",
+			Value:    0.5,
+			Required: false,
+		},
+		&cli.Float64Flag{
+			Name:     "y-frac",
+			Aliases:  []string{"y"},
+			Usage:    "Y fraction for cursor placement between 0 and 1, 0.5 denotes window center",
+			Value:    0.5,
+			Required: false,
+		},
+	}
+	app.Action = perform
+	return app
 }
 
 func main() {
 	logger = impl.NewLogger()
 	defer logger.Sync()
+	l := logger.Sugar()
 	impl.EnsureBinary("xdotool", *logger)
-	wg.Add(1)
-	go handleWindows()
-	wg.Wait()
+	app := createCLI()
+	err := app.Run(os.Args)
+	if err != nil {
+		l.Errorw("[main]", "err", err)
+	}
 }
